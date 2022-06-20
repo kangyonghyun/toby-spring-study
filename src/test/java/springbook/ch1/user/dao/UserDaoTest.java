e840d1264cc33ee3d0eecdab23fff218807fc534
kanghyonghyun <hmoon826@gmail.com>
2022-06-20 23:58:56 +0900

    [refactor] UserDao 테스트 수정

--- src/test/java/springbook/ch1/user/dao/UserDaoTest.java
+++ src/test/java/springbook/ch1/user/dao/UserDaoTest.java
@@ -1,7 +1,9 @@
 package springbook.ch1.user.dao;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import springbook.ch1.user.connection.ConnectionMaker;
 import springbook.ch1.user.connection.DriverManagerConnectionMaker;
 import springbook.ch1.user.domain.User;
@@ -15,20 +17,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 class UserDaoTest {
-    ConnectionMaker connectionMaker = new DriverManagerConnectionMaker();
-    UserDao userDao = new UserDao(connectionMaker);
+    SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+    UserDao userDao = new UserDao();
+
+    @BeforeEach
+    void before() {
+        dataSource.setDriverClass(org.h2.Driver.class);
+        dataSource.setUrl(ConnectionConst.URL);
+        dataSource.setUsername(ConnectionConst.USERNAME);
+        dataSource.setPassword(ConnectionConst.PASSWORD);
+    }
 
     @Test
     void connection() throws SQLException {
-        Connection con = DriverManager.getConnection(ConnectionConst.URL, ConnectionConst.USERNAME, ConnectionConst.PASSWORD);
+        Connection con = dataSource.getConnection();
         log.info("connection = {}, class = {}", con, con.getClass());
         assertThat(con).isNotNull();
     }
 
     @Test
     void crud() throws SQLException, ClassNotFoundException {
+        userDao.setDataSource(dataSource);
+
         User user = new User();
-        user.setId("kyh111");
+        user.setId("kyh11111");
         user.setName("yong");
         user.setPassword("test");
         userDao.add(user);
