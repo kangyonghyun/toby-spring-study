1cce47a2d17b8c9d00e4df8e9b8c9c1b2ff46e8e
kanghyonghyun <hmoon826@gmail.com>
2022-07-01 23:56:38 +0900

    [refactor] 스프링 JdbcTemplate 을 이용한 crud 작성

---
+++ src/main/java/springbook/ch2/user/dao/jdbctemplate/JdbcTemplateUserDao.java
@@ -0,0 +1,63 @@
+package springbook.ch2.user.dao.jdbctemplate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
+import springbook.ch2.user.domain.User;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class JdbcTemplateUserDao {
+
+    static final Logger log = LoggerFactory.getLogger(JdbcTemplateUserDao.class);
+    private JdbcTemplate jdbcTemplate;
+
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public void add(User user){
+        this.jdbcTemplate.update("insert into users(id, name, password) values (?, ?, ?)",
+                user.getId(), user.getName(), user.getPassword());
+    }
+
+    public User get(String id) {
+        return this.jdbcTemplate.queryForObject("select * from users where id = ?",
+                userRowMapper(), id);
+    }
+    public List<User> getAll() {
+        return this.jdbcTemplate.query("select * from users", userRowMapper());
+    }
+
+    public void deleteAll() {
+        this.jdbcTemplate.update("delete from users");
+    }
+
+    public int getCount() {
+        return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
+    }
+
+    private RowMapper<User> userRowMapper() {
+        return new RowMapper<User>() {
+            @Override
+            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                User user = new User();
+                user.setId(rs.getString("id"));
+                user.setName(rs.getString("name"));
+                user.setPassword(rs.getString("password"));
+                return user;
+            }
+        };
+    }
+
+}
