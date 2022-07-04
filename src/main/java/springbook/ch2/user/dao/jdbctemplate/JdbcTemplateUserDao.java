72c8ccd5876f7120212350850d41d282f6e044d9
kanghyonghyun <hmoon826@gmail.com>
2022-07-04 23:12:59 +0900

    [refactor] rowmapper 변수로 추출

--- src/main/java/springbook/ch2/user/dao/jdbctemplate/JdbcTemplateUserDao.java
+++ src/main/java/springbook/ch2/user/dao/jdbctemplate/JdbcTemplateUserDao.java
@@ -2,25 +2,24 @@ package springbook.ch2.user.dao.jdbctemplate;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 import springbook.ch2.user.domain.User;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 public class JdbcTemplateUserDao {
 
     static final Logger log = LoggerFactory.getLogger(JdbcTemplateUserDao.class);
     private JdbcTemplate jdbcTemplate;
+    private RowMapper<User> userRowMapper = (rs, rowNum) -> {
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+        return user;
+    };
 
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
@@ -33,11 +32,11 @@ public class JdbcTemplateUserDao {
 
     public User get(String id) {
         return this.jdbcTemplate.queryForObject("select * from users where id = ?",
-                userRowMapper(), id);
+                userRowMapper, id);
     }
 
     public List<User> getAll() {
-        return this.jdbcTemplate.query("select * from users order by id", userRowMapper());
+        return this.jdbcTemplate.query("select * from users order by id", userRowMapper);
     }
 
     public void deleteAll() {
@@ -46,19 +45,6 @@ public class JdbcTemplateUserDao {
 
     public int getCount() {
         return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
-    }
-
-    private RowMapper<User> userRowMapper() {
-        return new RowMapper<User>() {
-            @Override
-            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-                User user = new User();
-                user.setId(rs.getString("id"));
-                user.setName(rs.getString("name"));
-                user.setPassword(rs.getString("password"));
-                return user;
-            }
-        };
     }
 
 }
