070c137a3e16d4dffa16c5f15a62177dec4a72be
kanghyonghyun <hmoon826@gmail.com>
2022-07-07 22:53:41 +0900

    [test] upgradeLevel 테스트 추가

--- src/test/java/springbook/ch4/user/domain/UserTest.java
+++ src/test/java/springbook/ch4/user/domain/UserTest.java
@@ -1,10 +1,19 @@
 package springbook.ch4.user.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class UserTest {
+
+    User user;
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+    }
 
     @Test
     void create() {
@@ -16,6 +25,32 @@ class UserTest {
 
         User user3 = new User("kyh1", "kyh", "ps", Level.BASIC, 0, 0);
         assertThat(user3).isEqualTo(new User("kyh1", "kyh", "ps", Level.BASIC, 0, 0));
+    }
+
+    @Test
+    void nextLevel() {
+        Level[] levels = Level.values();
+        for (Level level : levels) {
+            user.setLevel(level);
+            if (level.nextLevel() == null) {
+                continue;
+            }
+            user.upgradeLevel();
+            assertThat(user.getLevel()).isEqualTo(level.nextLevel());
+        }
+    }
+
+    @Test
+    void no_nextLevel() {
+        Level[] levels = Level.values();
+        for (Level level : levels) {
+            user.setLevel(level);
+            if (level.nextLevel() != null) {
+                continue;
+            }
+            assertThatThrownBy(() -> user.upgradeLevel())
+                    .isInstanceOf(IllegalStateException.class);
+        }
     }
 
 }
--- src/test/java/springbook/ch4/user/service/UserServiceTest.java
+++ src/test/java/springbook/ch4/user/service/UserServiceTest.java
@@ -18,6 +18,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static springbook.ch4.user.service.UserService.*;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration("/jdbctemplateV3.xml")
@@ -33,10 +34,10 @@ class UserServiceTest {
 
     @BeforeEach
     void setUp() {
-        User user1 = new User("kyh1", "yong", "test", Level.BASIC, 50,30);
-        User user2 = new User("kyh2", "yong", "test", Level.BASIC, 49,30);
-        User user3 = new User("kyh3", "yong", "test", Level.SILVER, 55, 30);
-        User user4 = new User("kyh4", "yong", "test", Level.SILVER, 1, 29);
+        User user1 = new User("kyh1", "yong", "test", Level.BASIC, MIN_LOGIN_FOR_SILVER,30);
+        User user2 = new User("kyh2", "yong", "test", Level.BASIC, MIN_LOGIN_FOR_SILVER - 1,30);
+        User user3 = new User("kyh3", "yong", "test", Level.SILVER, 55, MIN_RECOMMEND_FOR_GOLD);
+        User user4 = new User("kyh4", "yong", "test", Level.SILVER, 1, MIN_RECOMMEND_FOR_GOLD - 1);
         User user5 = new User("kyh5", "yong", "test", Level.GOLD, 100, 50);
         users = List.of(user1, user2, user3, user4, user5);
     }
@@ -64,16 +65,25 @@ class UserServiceTest {
         }
         userService.upgradeLevels();
 
-        checkLevel(users.get(0), Level.SILVER);
-        checkLevel(users.get(1), Level.BASIC);
-        checkLevel(users.get(2), Level.GOLD);
-        checkLevel(users.get(3), Level.SILVER);
-        checkLevel(users.get(4), Level.GOLD);
+        checkLevelUpgraded(users.get(0), true);
+        checkLevelUpgraded(users.get(1), false);
+        checkLevelUpgraded(users.get(2), true);
+        checkLevelUpgraded(users.get(3), false);
+        checkLevelUpgraded(users.get(4), false);
     }
 
     private void checkLevel(User user, Level level) {
         User findUser = userDao.get(user.getId());
         assertThat(findUser.getLevel()).isEqualTo(level);
+    }
+
+    private void checkLevelUpgraded(User user, boolean upgraded) {
+        User findUser = userDao.get(user.getId());
+        if (upgraded) {
+            assertThat(findUser.getLevel()).isEqualTo(user.getLevel().nextLevel());
+        } else {
+            assertThat(findUser.getLevel()).isEqualTo(user.getLevel());
+        }
     }
 
 }
