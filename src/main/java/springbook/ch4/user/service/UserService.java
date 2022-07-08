1f988e8bfc00d596fd5b8becbde4141fba789a94
kanghyonghyun <hmoon826@gmail.com>
2022-07-08 23:18:44 +0900

    [refactor] 트랜잭션 경계설정 코드 및 트랜잭션 동기화 적용

--- src/main/java/springbook/ch4/user/service/UserService.java
+++ src/main/java/springbook/ch4/user/service/UserService.java
@@ -1,17 +1,24 @@
 package springbook.ch4.user.service;
 
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 import springbook.ch4.user.dao.UserDao;
 import springbook.ch4.user.domain.Level;
 import springbook.ch4.user.domain.User;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 public class UserService {
 
     public static final int MIN_LOGIN_FOR_SILVER = 50;
     public static final int MIN_RECOMMEND_FOR_GOLD = 30;
     private final UserDao userDao;
+    private final DataSource dataSource;
 
-    public UserService(UserDao userDao) {
+    public UserService(UserDao userDao, DataSource dataSource) {
         this.userDao = userDao;
+        this.dataSource = dataSource;
     }
 
     public void save(User user) {
@@ -21,10 +28,24 @@ public class UserService {
         userDao.add(user);
     }
 
-    public void upgradeLevels() {
-        userDao.getAll().stream()
-                .filter(this::canUpgradeLevel)
-                .forEach(this::upgradeLevel);
+    public void upgradeLevels() throws Exception {
+        TransactionSynchronizationManager.initSynchronization();
+        Connection c = DataSourceUtils.getConnection(dataSource);
+        c.setAutoCommit(false);
+
+        try {
+            userDao.getAll().stream()
+                    .filter(this::canUpgradeLevel)
+                    .forEach(this::upgradeLevel);
+            c.commit();
+        } catch (Exception e) {
+            c.rollback();
+            throw e;
+        } finally {
+            DataSourceUtils.releaseConnection(c, dataSource);
+            TransactionSynchronizationManager.unbindResource(dataSource);
+            TransactionSynchronizationManager.clearSynchronization();
+        }
     }
 
     private boolean canUpgradeLevel(User user) {
