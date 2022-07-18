66e56bde8b3a5280545e92370355616d36c8846c
kanghyonghyun <hmoon826@gmail.com>
2022-07-18 22:31:09 +0900

    [test] Reflection 테스트를 내부 클래스에서 외부 클래스로 분리

---
+++ src/test/java/springbook/ch5/user/learningtest/reflection/Hello.java
@@ -0,0 +1,7 @@
+package springbook.ch5.user.learningtest.reflection;
+
+public interface Hello {
+    String sayHello(String name);
+    String sayHi(String name);
+    String sayThankYou(String name);
+}
---
+++ src/test/java/springbook/ch5/user/learningtest/reflection/HelloTarget.java
@@ -0,0 +1,18 @@
+package springbook.ch5.user.learningtest.reflection;
+
+public class HelloTarget implements Hello {
+    @Override
+    public String sayHello(String name) {
+        return "Hello " + name;
+    }
+
+    @Override
+    public String sayHi(String name) {
+        return "Hi " + name;
+    }
+
+    @Override
+    public String sayThankYou(String name) {
+        return "Thank you " + name;
+    }
+}
