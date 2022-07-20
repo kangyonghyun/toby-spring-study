44d0cc1e5fd1f5ee82b9913dfe5e8df4c7ccc353
kanghyonghyun <hmoon826@gmail.com>
2022-07-20 22:43:36 +0900

    [test] 스프링 ProxyFactoryBean 을 이용해 프록시 생성 테스트

---
+++ src/test/java/springbook/ch5/user/learningtest/DynamicProxyTest.java
@@ -0,0 +1,70 @@
+package springbook.ch5.user.learningtest;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactoryBean;
+import springbook.ch5.user.learningtest.reflection.Hello;
+import springbook.ch5.user.learningtest.reflection.HelloTarget;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DynamicProxyTest {
+    
+    @Test
+    @DisplayName("다이내믹 프록시를 이용한 프록시 생성 테스트")
+    void simpleProxy() {
+        Hello hello = (Hello) Proxy.newProxyInstance(
+                getClass().getClassLoader(),
+                new Class[]{Hello.class},
+                new UppercaseHandler(new HelloTarget())
+        );
+        assertThat(hello.sayHello("toby")).isEqualTo("HELLO TOBY");
+        assertThat(hello.sayHi("toby")).isEqualTo("Hi toby");
+        assertThat(hello.sayThankYou("toby")).isEqualTo("Thank you toby");
+    }
+
+    static class UppercaseHandler implements InvocationHandler {
+        private final Hello hello;
+        public UppercaseHandler(Hello hello) {
+            this.hello = hello;
+        }
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            Object helloReturn = method.invoke(hello, args);
+            if (helloReturn instanceof String && method.getName().startsWith("sayHello")) {
+                return  ((String) helloReturn).toUpperCase();
+            } else {
+                return helloReturn;
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("스프링 다이내믹 프록시를 이용한 프록시 생성 테스트")
+    void proxyFactoryBean() {
+        ProxyFactoryBean proxyFactoryBean = new ProxyFactoryBean();
+        proxyFactoryBean.setTarget(new HelloTarget());
+        proxyFactoryBean.addAdvice(new UppercaseAdvice());
+
+        Hello hello = (Hello) proxyFactoryBean.getObject();
+
+        assertThat(hello.sayHello("Toby")).isEqualTo("HELLO TOBY");
+        assertThat(hello.sayHi("Toby")).isEqualTo("HI TOBY");
+        assertThat(hello.sayThankYou("Toby")).isEqualTo("THANK YOU TOBY");
+    }
+
+    static class UppercaseAdvice implements MethodInterceptor {
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            String ret = (String) invocation.proceed();
+            return ret.toUpperCase();
+        }
+    }
+
+}
