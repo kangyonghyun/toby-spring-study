e4cc201e70153134405c63a7ca8182be561a9979
kanghyonghyun <hmoon826@gmail.com>
2022-07-28 23:54:51 +0900

    [test] @Transactional 애노테이션 적용

--- src/main/java/springbook/ch5/user/service/UserService.java
+++ src/main/java/springbook/ch5/user/service/UserService.java
@@ -1,14 +1,18 @@
 package springbook.ch5.user.service;
 
+import org.springframework.transaction.annotation.Transactional;
 import springbook.ch5.user.domain.User;
 
 import java.util.List;
 
+@Transactional
 public interface UserService {
     void save(User user);
+    @Transactional(readOnly = true)
     User get(String id);
+    @Transactional(readOnly = true)
     List<User> getAll();
     void deleteAll();
     void upgradeLevels();
-
+    void update(User user);
 }
