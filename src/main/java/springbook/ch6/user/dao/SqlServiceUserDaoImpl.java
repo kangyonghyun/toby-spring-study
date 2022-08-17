698e91f896460a1b18a49e802ccf1b2953c27428
kanghyonghyun <hmoon826@gmail.com>
2022-08-17 22:55:00 +0900

    [feat] SQL 서비스 인터페이스 도입

---
+++ src/main/java/springbook/ch6/user/dao/SqlServiceUserDaoImpl.java
@@ -0,0 +1,69 @@
+package springbook.ch6.user.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import springbook.ch6.user.domain.Level;
+import springbook.ch6.user.domain.User;
+import springbook.ch6.user.sqlservice.SqlService;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+public class SqlServiceUserDaoImpl implements UserDao {
+
+    SqlService sqlService;
+    private final JdbcTemplate jdbcTemplate;
+
+    public SqlServiceUserDaoImpl(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public void setSqlService(SqlService sqlService) {
+        this.sqlService = sqlService;
+    }
+
+    private final RowMapper<User> userRowMapper = (rs, rowNum) -> {
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+        user.setLevel(Level.valueOf(rs.getInt("level")));
+        user.setLogin(rs.getInt("login"));
+        user.setRecommend(rs.getInt("recommend"));
+        user.setEmail(rs.getString("email"));
+        return user;
+    };
+
+    @Override
+    public void add(User user) {
+        jdbcTemplate.update(sqlService.getSql("add"),
+                user.getId(), user.getName(), user.getPassword(), user.getEmail(), user.getLevel().intValue(), user.getLogin(), user.getRecommend());
+    }
+
+    @Override
+    public User get(String id) {
+        return jdbcTemplate.queryForObject(sqlService.getSql("get"),
+                userRowMapper, id);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return jdbcTemplate.query(sqlService.getSql("getAll"), userRowMapper);
+    }
+
+    @Override
+    public void deleteAll() {
+        jdbcTemplate.update(sqlService.getSql("deleteAll"));
+    }
+
+    @Override
+    public int getCount() {
+        return this.jdbcTemplate.queryForObject(sqlService.getSql("getCount"), Integer.class);
+    }
+
+    @Override
+    public void update(User user) {
+        this.jdbcTemplate.update(sqlService.getSql("update"),
+                user.getName(), user.getPassword(), user.getLevel().intValue(), user.getLogin(), user.getRecommend(), user.getId());
+    }
+}
