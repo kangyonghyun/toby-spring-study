8997a0e8470f7f08bbdb2d0ca128bab9a6bb2a52
kanghyonghyun <hmoon826@gmail.com>
2022-08-23 23:39:53 +0900

    [refactor] 위임을 통해 중복코드 제거

---
+++ src/main/java/springbook/ch6/user/sqlservice/OxmSqlServiceV2.java
@@ -0,0 +1,71 @@
+package springbook.ch6.user.sqlservice;
+
+import org.springframework.oxm.Unmarshaller;
+import springbook.ch6.user.dao.UserDao;
+import springbook.ch6.user.service.SqlRetrievalFailureException;
+import springbook.ch6.user.sqlservice.jaxb.SqlType;
+import springbook.ch6.user.sqlservice.jaxb.Sqlmap;
+
+import javax.annotation.PostConstruct;
+import javax.xml.transform.stream.StreamSource;
+import java.io.IOException;
+
+public class OxmSqlServiceV2 implements SqlService {
+
+    private final BaseSqlService baseSqlService = new BaseSqlService();
+
+    private final OxmSqlReader oxmSqlReader = new OxmSqlReader();
+    private SqlRegistry sqlRegistry = new HashMapSqlRegistry();
+
+    public void setSqlRegistry(SqlRegistry sqlRegistry) {
+        this.sqlRegistry = sqlRegistry;
+    }
+
+    public void setUnmarshaller(Unmarshaller unmarshaller) {
+        this.oxmSqlReader.setUnmarshaller(unmarshaller);
+    }
+
+    public void setSqlmapFile(String sqlmapFile) {
+        this.oxmSqlReader.setSqlmapFile(sqlmapFile);
+    }
+
+    @PostConstruct
+    private void loadSql() {
+        this.baseSqlService.setSqlRegistry(this.sqlRegistry);
+        this.baseSqlService.setSqlReader(this.oxmSqlReader);
+
+        this.baseSqlService.loadSql();
+    }
+
+    @Override
+    public String getSql(String key) throws SqlRetrievalFailureException {
+        return this.baseSqlService.getSql(key);
+    }
+
+    private class OxmSqlReader implements SqlReader {
+        private Unmarshaller unmarshaller;
+
+        private final static String DEFAULT_SQLMAP_FILE = "/sqlmap2.xml";
+        private String sqlmapFile = DEFAULT_SQLMAP_FILE;
+
+        public void setUnmarshaller(Unmarshaller unmarshaller) {
+            this.unmarshaller = unmarshaller;
+        }
+
+        public void setSqlmapFile(String sqlmapFile) {
+            this.sqlmapFile = sqlmapFile;
+        }
+        @Override
+        public void read(SqlRegistry sqlRegistry) {
+            try {
+                StreamSource source = new StreamSource(UserDao.class.getResourceAsStream(this.sqlmapFile));
+                Sqlmap sqlmap = (Sqlmap) this.unmarshaller.unmarshal(source);
+                for (SqlType sql : sqlmap.getSql()) {
+                    sqlRegistry.registerSql(sql.getKey(), sql.getValue());
+                }
+            } catch (IOException e) {
+                throw new IllegalArgumentException(this.sqlmapFile + " 을 가져올 수 없습니다", e);
+            }
+        }
+    }
+}
