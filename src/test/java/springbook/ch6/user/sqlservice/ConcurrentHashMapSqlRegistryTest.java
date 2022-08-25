118f5947f5211723ccb6443f04ebcb8893f66301
kanghyonghyun <hmoon826@gmail.com>
2022-08-26 00:08:32 +0900

    [test] SqlRegistry 메모리 디비로 전환

--- src/test/java/springbook/ch6/user/sqlservice/ConcurrentHashMapSqlRegistryTest.java
+++ src/test/java/springbook/ch6/user/sqlservice/ConcurrentHashMapSqlRegistryTest.java
@@ -1,63 +1,10 @@
 package springbook.ch6.user.sqlservice;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+class ConcurrentHashMapSqlRegistryTest extends AbstractUpdatableSqlRegistryTest {
 
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
-class ConcurrentHashMapSqlRegistryTest {
-
-    UpdatableSqlRegistry sqlRegistry;
-
-    @BeforeEach
-    void setUp() {
-        sqlRegistry = new ConcurrentHashMapSqlRegistry();
-        sqlRegistry.registerSql("KEY1", "SQL1");
-        sqlRegistry.registerSql("KEY2", "SQL2");
-        sqlRegistry.registerSql("KEY3", "SQL3");
-    }
-
-    @Test
-    void find() {
-        checkFindResult("SQL1", "SQL2", "SQL3");
-    }
-
-    private void checkFindResult(String expected1, String expected2, String expected3) {
-        assertThat(sqlRegistry.findSql("KEY1")).isEqualTo(expected1);
-        assertThat(sqlRegistry.findSql("KEY2")).isEqualTo(expected2);
-        assertThat(sqlRegistry.findSql("KEY3")).isEqualTo(expected3);
-    }
-
-    @Test
-    void unKnownKey() {
-        assertThatThrownBy(() -> sqlRegistry.findSql("#KEY1"))
-                .isInstanceOf(SqlNotFoundException.class);
-    }
-
-    @Test
-    void updateSingle() {
-        sqlRegistry.updateSql("KEY1", "USQL1");
-        checkFindResult("USQL1", "SQL2", "SQL3");
-    }
-
-    @Test
-    void unknownKey_update() {
-        assertThatThrownBy(() -> sqlRegistry.updateSql("@KEY1", "USQL1"))
-                .isInstanceOf(SqlUpdateFailureException.class);
-    }
-
-    @Test
-    void updateMulti() {
-        Map<String, String> sqlmap = new HashMap<>();
-        sqlmap.put("KEY1", "KSQL1");
-        sqlmap.put("KEY3", "KSQL3");
-        sqlRegistry.updateSql(sqlmap);
-
-        checkFindResult("KSQL1", "SQL2", "KSQL3");
+    @Override
+    protected UpdatableSqlRegistry createUpdatableSqlRegistry() {
+        return new ConcurrentHashMapSqlRegistry();
     }
 
 }
