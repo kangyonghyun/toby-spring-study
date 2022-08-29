688dc02f2760c7deca8b36b251a77569a7423048
kanghyonghyun <hmoon826@gmail.com>
2022-08-29 23:09:54 +0900

    [feat] SqlRegistry 트랜잭션 적용

--- src/main/java/springbook/ch6/user/sqlservice/EmbeddedDbSqlRegistry.java
+++ src/main/java/springbook/ch6/user/sqlservice/EmbeddedDbSqlRegistry.java
@@ -2,15 +2,21 @@ package springbook.ch6.user.sqlservice;
 
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.sql.DataSource;
 import java.util.Map;
 
 public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry {
     private JdbcTemplate jdbc;
+    TransactionTemplate transactionTemplate;
 
     public void setDataSource(DataSource dataSource) {
         this.jdbc = new JdbcTemplate(dataSource);
+        this.transactionTemplate = new TransactionTemplate(new DataSourceTransactionManager(dataSource));
     }
 
     @Override
@@ -36,9 +42,14 @@ public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry {
     }
 
     @Override
-    public void updateSql(Map<String, String> sqlmap) throws SqlUpdateFailureException {
-        for (Map.Entry<String, String> entry : sqlmap.entrySet()) {
-            updateSql(entry.getKey(), entry.getValue());
-        }
+    public void updateSql(final Map<String, String> sqlmap) throws SqlUpdateFailureException {
+        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+                for (Map.Entry<String, String> entry : sqlmap.entrySet()) {
+                    updateSql(entry.getKey(), entry.getValue());
+                }
+            }
+        });
     }
 }
--- src/test/java/springbook/ch6/user/sqlservice/EmbeddedDbSqlRegistryTest.java
+++ src/test/java/springbook/ch6/user/sqlservice/EmbeddedDbSqlRegistryTest.java
@@ -1,9 +1,15 @@
 package springbook.ch6.user.sqlservice;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest {
 
@@ -24,5 +30,19 @@ class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest {
     @AfterEach
     void tearDown() {
         db.shutdown();
+    }
+
+    @Test
+    void transactionalUpdate() {
+        checkFindResult("SQL1", "SQL2", "SQL3");
+
+        Map<String, String> sqlmap = new HashMap<>();
+        sqlmap.put("KEY1", "Modified1");
+        sqlmap.put("KEY999!@#$", "Modified9999");
+
+        assertThatThrownBy(() -> sqlRegistry.updateSql(sqlmap))
+                .isInstanceOf(SqlUpdateFailureException.class);
+
+        checkFindResult("SQL1", "SQL2", "SQL3");
     }
 }
